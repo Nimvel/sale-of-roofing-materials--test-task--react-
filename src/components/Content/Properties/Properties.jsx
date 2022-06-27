ae672e106d6f554f464ff4782e229023c8acdadc
Nimvel <rabbit.of.margery@gmail.com>
2022-06-27 14:14:45 +0300

    mask

--- src/components/Content/Properties/Properties.jsx
+++ src/components/Content/Properties/Properties.jsx
@@ -3,8 +3,8 @@ import style from '../Content.module.css';
 
 const Properties = ({propertyStyle}) => {
     return (
-        <div className={style[propertyStyle]}>
-            {/* <div className={style.remove} /> */}
+        <div className={style.properties}>
+            <div className={style.remove} />
             <div className={style.first_property} ><strong>Гарантия от 50 лет</strong> на материалы и 5 лет на монтаж</div>
             <div className={style.second_property}><strong>Замер, монтажный проект</strong> и<br />еще 4 услуги бесплатно</div>
             <div className={style.third_property}><strong>Сметы<br />точны</strong> до копейки </div>
--- src/components/Submit_application/SubmitApplication.jsx
+++ src/components/Submit_application/SubmitApplication.jsx
@@ -1,10 +1,13 @@
 import style from './SubmitApplication.module.css';
 import close from '../../assets/close.svg';
 import { NavLink } from 'react-router-dom';
+import React from 'react';
+import ReactInputMask from 'react-input-mask';
 // import checkbox from '../../assets/checkbox.svg'
 // import empty from '../../assets/empty.svg'
 
-const SubmitApplication = ({ checkboxStyle, checkbox_click}) => {
+const SubmitApplication = ({ checkboxStyle, checkbox_click }) => {
+    const [phone, setPhone] = React.useState("");
     let onCheckboxClick = () => {
         checkbox_click();
     }
@@ -18,7 +21,12 @@ const SubmitApplication = ({ checkboxStyle, checkbox_click}) => {
                 <div className={style.enter_number_text}>Введите номер, мы позвоним вам в<br />течение 10 минут в рабочее время</div>
                 <div className={style.schedule} >Пн-Пт 9:00 - 18:00, Сб 10:00 - 18:00</div>
                 <div className={style.enter_number}>Введите номер телефона</div>
-                <input placeholder="+7(___)__-__-__" />
+                {/* <input /> */}
+                <ReactInputMask value={phone}
+                    onChange={e => setPhone(e.target.value)}
+                    mask="+7(999)99-99-99"
+                    maskChar=" "
+                    placeholder='+7(___)__-__-__' />
                 <div className={style.call_me_button}>
                     <span className={style.call_me_button_text}>Позвоните мне</span>
                 </div>
